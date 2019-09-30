d588653f57ab0fae85e1dd4a01b21bcc7ca981e0
SubasreeArvind <43107916+SubasreeArvind@users.noreply.github.com>
2019-09-30 12:08:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ class EchoBot extends ActivityHandler {
          
          
             //  await context.sendActivity(`You said '${ context.activity.text }'`);
-            await context.sendActivity(context.activity.text.reverse());
+            await context.sendActivity(context.activity.text.split('').reverse().join(''));
 
 
             // By calling next() you ensure that the next BotHandler is run.
